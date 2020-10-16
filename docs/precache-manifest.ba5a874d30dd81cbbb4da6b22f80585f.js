06b113cf1f36b30a1038c75d1ec8f18983ebe5fb
Andres Cedeño <cedenoandres18@gmail.com>
2020-10-15 21:44:15 -0600

    docs agregado

---
+++ docs/precache-manifest.ba5a874d30dd81cbbb4da6b22f80585f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d298317035eb2a43e3f44f5631e132b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "062bdc7426b8cd41462d",
+    "url": "/static/css/main.07f0968e.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062bdc7426b8cd41462d",
+    "url": "/static/js/main.1f49c64a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
